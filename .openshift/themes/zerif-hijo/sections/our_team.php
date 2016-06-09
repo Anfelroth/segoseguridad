7412c6a168050747fa4bde1eb26fdb618d181368
anfelroth <andresfboco@hotmail.com>
2016-06-09 07:46:08 -0500

    Replace ourteam section3

--- .openshift/themes/zerif-hijo/sections/our_team.php
+++ .openshift/themes/zerif-hijo/sections/our_team.php
@@ -1,1 +1,19 @@
-    <?php echo do_shortcode('[foogallery id="35"]'); ?>
+    <?
+    <echo '<section class="our-team" id="team">';
+
+				echo '<div class="container">';
+
+					echo '<div class="section-header">';
+
+						$zerif_ourteam_title = get_theme_mod('zerif_ourteam_title',__(' NUESTROS PRODUCTOS','zerif-lite'));
+					
+						if( !empty($zerif_ourteam_title) ):
+							echo '<h2 class="dark-text">'.$zerif_ourteam_title.'</h2>';
+						endif;
+
+						
+
+					echo '</div>';
+
+    php echo do_shortcode('[foogallery id="35"]'); 
+    ?>
